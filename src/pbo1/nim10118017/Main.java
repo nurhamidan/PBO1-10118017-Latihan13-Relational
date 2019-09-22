1e8f32ff3e7f9555291e7eb49d77194bf3e02bf2
Agung Nurhamidan <>
2019-09-22 18:18:30 +0700

    First commit.

---
+++ src/pbo1/nim10118017/Main.java
@@ -0,0 +1,28 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Mendeskripsikan variabel serta menampilkannya dalam bentuk operasi relasi di layar.
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("b >= a = " + (b >= a));
+        System.out.println("b <= a = " + (b <= a));
+    }
+    
+}
